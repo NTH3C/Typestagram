a618a2b92cb90f9235c4d701f9e54ecb04a27109
akik4 <dupontstef80@gmail.com>
2026-02-10 09:57:14 +0100

    feat(homepage): creating home page

--- typestagram/src/App.tsx
+++ typestagram/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 
 import ProfilePage from './pages/myProfilePage';
 
 import Register from './pages/register';
 import Login from './pages/login';
-import { Box, Button as ButtonIcon } from '@mui/material';
+import { Box, Button as ButtonIcon, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
+import type { User } from './types/Profile';
+import { useEffect } from 'react';
 
 
 function App() {
@@ -30,6 +32,7 @@ function App() {
 
         <Route path="/register" element={<Register />}/>
         <Route path="/login" element={<Login />}/>
+        <Route path="/logout" element={<Logout />}/>
 
         {/* <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} /> */}
@@ -38,12 +41,37 @@ function App() {
   );
 }
 
+function Logout() {
+  const storedUser = localStorage.getItem("user");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (storedUser) {
+      localStorage.clear()
+    }
+  
+    navigate("/")    
+  })
+}
+
 function MyApp() {
+  
+  const storedUser = localStorage.getItem("user");
 
   return (
     <>
       <Box>
-        <Button variant="contained" href="/login">Login</Button>
+      {
+        storedUser ? (
+          <Button variant="contained" sx={{background: "red"}} href="/logout">
+            se déconnecter
+          </Button>// ou ce que tu veux afficher quand l'utilisateur est connecté
+        ) : (
+          <Button variant="contained" href="/login">
+            Login
+          </Button>
+        )
+      }
       </Box>
       <h1>Accueil</h1>   
 
