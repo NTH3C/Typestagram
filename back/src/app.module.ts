a16fcfe33285262e370226385219f81a7b2eed39
akik4 <dupontstef80@gmail.com>
2026-02-10 13:34:07 +0100

    fix(like): missing module import

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostsModule } from './posts/posts.module';
+import { LikeModule } from './like/like.module';
 
 @Module({
-  imports: [AuthModule, PostsModule, UserModule],
+  imports: [AuthModule, PostsModule, UserModule, LikeModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- back/src/like/like.controller.ts
+++ back/src/like/like.controller.ts
@@ -1,17 +1,17 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { LikeService, Like } from './like.service';
+import * as likeService from './like.service';
 
 @Controller('likes')
 export class LikeController {
-  constructor(private readonly likeService: LikeService) {}
+  constructor(private readonly likeService: likeService.LikeService) {}
 
   @Get()
-  getFeed(): Like[] {
+  getFeed(): likeService.Like[] {
     return this.likeService.getFeed();
   }
 
   @Post()
-  likePost(@Body() post: Omit<Like, 'id'>): Like {
+  likePost(@Body() post: Omit<likeService.Like, 'id'>): likeService.Like {
     return this.likeService.likePost(post);
   }
 }
